8d5664f57b3d3aa446eecd07af0f9cc5543fa426
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-02-12 13:54:13 +0200

    abd

--- test.go
+++ test.go
@@ -1,3 +1,55 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"os"
+// 	"strings"
+// )
+
+// func moaz(text string) string {
+// 	var s int
+// 	fmt.Println("choose a font 😊")
+// 	fmt.Println("standard = 1")
+// 	fmt.Println("shadow = 2")
+// 	fmt.Println("thinkertoy = 3")
+// 	fmt.Scan(&s)
+// 	var m2 []string
+// 	var m1 []string
+// 	for i := 0; i < len(text); i++ {
+// 		switch s {
+// 		case 1:
+// 			m2 = standard(text[i])
+// 		case 2:
+// 			m2 = shadow(text[i])
+// 		case 3:
+// 			m2 = thinkertoy(text[i])
+// 		}
+// 		if i == 0 {
+// 			m1 = m2
+// 		} else {
+// 			for j := 0; j < len(m1); j++ {
+// 				m1[j] += m2[j]
+// 			}
+// 		}
+// 	}
+// 	return strings.Join(m1, "\n")
+// }
+
+// func main() {
+// 	if len(os.Args) != 2 {
+// 		fmt.Println("Usage: go run . \"{YourText}\"")
+// 		return
+// 	}
+// 	inputText := os.Args[1]
+// 	lines := strings.Split(strings.TrimSpace(inputText), "\\n")
+// 	asciiArt := []string{}
+// 	for _, line := range lines {
+// 		asciiArt = append(asciiArt, moaz(line))
+// 	}
+// 	asciiArtText := strings.Join(asciiArt, "\n")
+// 	fmt.Println(asciiArtText)
+// }
+
 package main
 
 import (
@@ -6,13 +58,20 @@ import (
 	"strings"
 )
 
-func moaz(text string) string {
+func moaz(text string, fontType string) (string, string) {
 	var s int
-	fmt.Println("choose a font 😊")
-	fmt.Println("standard = 1")
-	fmt.Println("shadow = 2")
-	fmt.Println("thinkertoy = 3")
-	fmt.Scan(&s)
+	switch fontType {
+	case "standard":
+		s = 1
+	case "shadow":
+		s = 2
+	case "thinkertoy":
+		s = 3
+	default:
+		fmt.Println("Invalid font type. Using standard font.")
+		s = 1
+	}
+
 	var m2 []string
 	var m1 []string
 	for i := 0; i < len(text); i++ {
@@ -32,19 +91,18 @@ func moaz(text string) string {
 			}
 		}
 	}
-	return strings.Join(m1, "\n")
+	return strings.Join(m1, "\n"), fontType
 }
+
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: go run . \"{YourText}\"")
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: go run . \"{YourText}\" {FontType}")
 		return
 	}
 	inputText := os.Args[1]
-	lines := strings.Split(strings.TrimSpace(inputText), "\\n")
-	asciiArt := []string{}
-	for _, line := range lines {
-		asciiArt = append(asciiArt, moaz(line))
-	}
-	asciiArtText := strings.Join(asciiArt, "\n")
-	fmt.Println(asciiArtText)
+	fontType := os.Args[2]
+
+	art, chosenFontType := moaz(inputText, fontType)
+	fmt.Println("Font Type:", chosenFontType)
+	fmt.Println(art)
 }
